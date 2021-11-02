a83b8b5a2a4d66b4d0f3a1682c9cd525bb192453
isaQuant <isabella.quantschnig@gmx.net>
2021-11-02 11:29:47 +0100

    created learning project for javascript learning tasks

---
+++ variables.js
@@ -0,0 +1,25 @@
+
+// initialization of variables
+var a;
+let b = 2;
+
+console.log(a)
+console.log(b)
+
+var x = 5;
+var y = 10;
+var z = "I am a";
+
+x = x +1;
+y = y + 5;
+z = z + " String! ";
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+// case sensitivity in variables
+var properCamelCase;
+PRoperCamelCAse = "A String";
+console.log(properCamelCase)
+console.log(PRoperCamelCAse)
