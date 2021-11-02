60dd9687762d35abf3a84788f9953cca2f8fb434
isaQuant <isabella.quantschnig@gmx.net>
2021-11-02 15:36:53 +0100

    added arrays

---
+++ Arrays.js
@@ -0,0 +1,23 @@
+// nested array / multidimensional array
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+var myArray = ourArray[0];
+console.log(myArray)
+
+// push and pop
+// push
+var arr1 = ["Stimpson", "J", "cat"];
+arr1.push(["happy", "joy"]);
+console.log(arr1)
+
+
+// pop
+var arr = [1, 2, 3];
+var removedFromArr = arr.pop();
+// removedFromArr now equals 3 and arr now equals [1,2]
+console.log(arr)
+console.log(removedFromArr)
+
+
+
+
