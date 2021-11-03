09d3c3dc6e746bb97025f7e53ae1eb6a2e325dba
isaQuant <isabella.quantschnig@gmx.net>
2021-11-03 20:27:49 +0100

    added function for calculating the sum of two numbers

---
+++ SumOfTwoNumbers.js
@@ -0,0 +1,11 @@
+/**
+ * Calculates the sum of two numbers
+ * @param num1 first number
+ * @param num2 second number
+ * @returns the sum of num1 and num2
+ */
+function sum(num1, num2) {
+    return num1+num2;
+}
+
+console.log(sum(2,15));
