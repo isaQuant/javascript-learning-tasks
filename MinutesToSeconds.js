250e2caa951b921e0e8b0f43bc054ee58f4a459e
isaQuant <isabella.quantschnig@gmx.net>
2021-11-03 20:38:11 +0100

    added function which converts minutes to seconds

---
+++ MinutesToSeconds.js
@@ -0,0 +1,10 @@
+/**
+ * Converts minutes to seconds
+ * @param minutes
+ * @returns seconds
+ */
+function minutesToSeconds(minutes) {
+    return minutes/60;
+}
+
+console.log(minutesToSeconds(120));
